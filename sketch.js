4c9f99437311f7bb0167975b6b8047d29f9400b0
monk645 <75474612+monk645@users.noreply.github.com>
2021-07-30 15:33:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var form, player, game;
+var allPlayers;
+var car1, car2,car3, car4, cars = [];
+
+function setup() {
+  canvas = createCanvas(displayWidth - 50, displayHeight - 160);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw() {
+  if (playerCount === 4) {
+    game.update(1);
+  }
+  if (gameState === 1) {
+    game.play();
+  }
+}
